173bdd7ee47f233ee503e334ae01e78628867a66
ekorch <johnkorch@yandex.ru>
2018-01-28 17:49:53 +0300

    HW1 - Korchevoi Eugene

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,27 +4,58 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    private static int ELEMENTS_COUNTER = 0;
+
     void clear() {
+        storage = new Resume[10000];
+        ELEMENTS_COUNTER = 0;
     }
 
     void save(Resume r) {
+        storage[ELEMENTS_COUNTER] = r;
+        ELEMENTS_COUNTER++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume searchResult = new Resume();
+        searchResult.uuid = "There is no matches!";
+        for (Resume elementOfArray : storage) {
+            if (elementOfArray == null) {
+                break;
+            } else if (elementOfArray.uuid.equals(uuid)) {
+                searchResult = elementOfArray;
+                break;
+            }
+        }
+        return searchResult;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);
+                ELEMENTS_COUNTER--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] currentStorage = new Resume[ELEMENTS_COUNTER];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            } else {
+                currentStorage[i] = storage[i];
+            }
+        }
+        return currentStorage;
     }
 
     int size() {
-        return 0;
+        return ELEMENTS_COUNTER;
     }
 }
