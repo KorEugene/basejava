f69073d51c65bce481fbf0b0afe775dadee74eb3
ekorch <johnkorch@yandex.ru>
2018-06-11 18:38:21 +0300

    Lesson 14.2

--- src/com/urise/webapp/sql/SqlHelper.java
+++ src/com/urise/webapp/sql/SqlHelper.java
@@ -15,12 +15,6 @@ public class SqlHelper {
 
     public void execute(String sql) {
         execute(sql, PreparedStatement::execute);
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
     }
 
     public <T> T execute(String sql, SqlExecutor<T> executor) {
@@ -29,6 +23,22 @@ public class SqlHelper {
             return executor.execute(ps);
         } catch (SQLException e) {
             throw ExceptionUtil.convertException(e);
+        }
+    }
+
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T res = executor.execute(conn);
+                conn.commit();
+                return res;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
 }
