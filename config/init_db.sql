37b045027e32edb1be07472b4925a25e4685ecdf
ekorch <johnkorch@yandex.ru>
2018-06-13 23:56:42 +0300

    HW 15.1

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,18 +1,28 @@
-create table if not exists resume
+CREATE TABLE IF NOT EXISTS resume
 (
-  uuid      char(36) not null constraint resume_pkey primary key,
-  full_name text     not null
+  uuid      CHAR(36) NOT NULL CONSTRAINT resume_pkey PRIMARY KEY,
+  full_name TEXT     NOT NULL
 );
 
-create table if not exists contact
+CREATE TABLE IF NOT EXISTS contact
 (
-  id          serial   not null constraint contact_pkey primary key,
-  resume_uuid char(36) not null constraint contact_resume_uuid_fk references resume on delete cascade,
-  type        text     not null,
-  value       text     not null
+  id          SERIAL   NOT NULL CONSTRAINT contact_pkey PRIMARY KEY,
+  resume_uuid CHAR(36) NOT NULL CONSTRAINT contact_resume_uuid_fk REFERENCES resume ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
 );
 
 CREATE UNIQUE INDEX contact_uuid_type_index
   ON public.contact (resume_uuid, type);
 
+CREATE TABLE IF NOT EXISTS section
+(
+  id          SERIAL PRIMARY KEY,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  content     TEXT     NOT NULL
+);
+
+CREATE UNIQUE INDEX section_idx
+  ON contact (resume_uuid, type);
 
--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -4,23 +4,17 @@ import com.urise.webapp.Config;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.ContactType;
-import com.urise.webapp.model.ListSection;
-import com.urise.webapp.model.Organization;
-import com.urise.webapp.model.OrganizationSection;
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.model.SectionType;
-import com.urise.webapp.model.StringSection;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.time.Month;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
+import static com.urise.webapp.TestData.*;
 import static org.junit.Assert.assertEquals;
 
 public class AbstractStorageTest {
@@ -29,50 +23,6 @@ public class AbstractStorageTest {
 
     protected Storage storage;
 
-    private static final String UUID_1 = UUID.randomUUID().toString();
-    private static final String UUID_2 = UUID.randomUUID().toString();
-    private static final String UUID_3 = UUID.randomUUID().toString();
-    private static final String UUID_4 = UUID.randomUUID().toString();
-
-    private static final Resume R1;
-    private static final Resume R2;
-    private static final Resume R3;
-    private static final Resume R4;
-
-    static {
-        R1 = new Resume(UUID_1, "Name1");
-        R2 = new Resume(UUID_2, "Name2");
-        R3 = new Resume(UUID_3, "Name3");
-        R4 = new Resume(UUID_4, "Name4");
-
-        R1.addContact(ContactType.E_MAIL, "mail1@ya.ru");
-        R1.addContact(ContactType.PHONE_NUMBER, "11111");
-        R4.addContact(ContactType.PHONE_NUMBER, "4444");
-        R4.addContact(ContactType.SKYPE, "Skype");
-        /*
-        R1.addSection(SectionType.PERSONAL, new StringSection("Personal data"));
-        R1.addSection(SectionType.OBJECTIVE, new StringSection("Objective1"));
-        R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achievement11", "Achievement12", "Achievement13"));
-        R1.addSection(SectionType.QUALIFICATIONS, new ListSection("Java", "SQL", "JavaScript"));
-        R1.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Organization11", "http://Organization11.ru",
-                                new Organization.Position(2005, Month.JANUARY, "position1", "content1"),
-                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "position2", "content2"))));
-        R1.addSection(SectionType.EDUCATION,
-                new OrganizationSection(
-                        new Organization("Institute", null,
-                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
-                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "student", "IT faculty")),
-                        new Organization("Organization12", "http://Organization12.ru")));
-        R2.addContact(ContactType.SKYPE, "skype2");
-        R2.addContact(ContactType.PHONE_NUMBER, "22222");
-        R1.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Organization2", "http://Organization2.ru",
-                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));*/
-
-    }
 
     protected AbstractStorageTest(Storage storage) {
         this.storage = storage;
