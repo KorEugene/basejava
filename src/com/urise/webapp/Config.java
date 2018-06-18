81e46424416b7652aa9a58b66c9e2e1c8b9b031f
ekorch <johnkorch@yandex.ru>
2018-06-18 14:07:14 +0300

    HW 15.2

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -10,8 +10,8 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
+    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
     private static final Config INSTANCE = new Config();
-    private static final File PROPS = new File("./storage/resumes.properties");
     private final File storageDir;
     private final Storage storage;
 
@@ -20,7 +20,7 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream("./config/resumes.properties")) {
+        try (InputStream is = new FileInputStream(PROPS)) {
             Properties properties = new Properties();
             properties.load(is);
             storageDir = new File(properties.getProperty("storage.dir"));
@@ -36,5 +36,14 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory!");
+        }
+        return homeDir;
     }
 }
