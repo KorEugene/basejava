54b744d1c03187a7c35fe29bf98985089637e72c
ekorch <johnkorch@yandex.ru>
2018-06-19 21:52:16 +0300

    Lesson 16.4

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -2,8 +2,18 @@ package com.urise.webapp.model;
 
 public enum ContactType {
     PHONE_NUMBER("Телефон"),
-    SKYPE("Skype"),
-    E_MAIL("Электронная почта"),
+    SKYPE("Skype") {
+        @Override
+        public String toHtml0(String value) {
+            return "<a href='skype:" + value + ">" + value + "</a>";
+        }
+    },
+    E_MAIL("Электронная почта") {
+        @Override
+        public String toHtml0(String value) {
+            return "<a href='mailto:" + value + ">" + value + "</a>";
+        }
+    },
     LINKEDIN("Профиль LinkedIn"),
     GITHUB("Профиль GitHub"),
     STACKOVERFLOW("Профиль StackOverFlow"),
@@ -17,5 +27,13 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    protected String toHtml0(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "" : toHtml0(value);
     }
 }
